6be361f8c1a3582322c0de1f970caeb670c3bb57
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-08 12:16:10 +0200

    grid-problem

--- grid.js
+++
@@ -1,4 +0,0 @@
-// Lets say that we are traveling on a 2D grid. We begin in the top-left
-// corner and our target is to travel to the bottom-right corner.
-// We can only move down or right.(no other moves to do)
-
---
+++ gridTraveler.js
@@ -0,0 +1,24 @@
+// Lets say that we are traveling on a 2D grid. We begin in the top-left
+// corner and our target is to travel to the bottom-right corner.
+// We can only move down or right.(no other moves to do)
+
+// In how many ways can you travel to the target on a grid with dimensions m * n?
+// your function gridTraveler(m,n) will calculate this trip.
+
+//Methodology Approach
+// 1. if our function is gridTraveler(2,3) means how we will travel in a grid with dimensions
+// 2 by 3? There are 3 ways to travel.
+// 3. if the function is griTraveler(1,1) means there is 1 way of traveling
+// 4. And if the function is gridTraveler(0,1) or(1,0) or(0,0) then means the grid is empty.
+//5. Lets calculate now gridTraveler(3,3) so we want to travel in a grid with dimensions 3 by 3
+//                
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
