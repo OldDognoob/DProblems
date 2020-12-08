28f6b561930faabe908570b93b8923518d031269
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-08 17:42:16 +0200

    grid-problem

--- gridTraveler.js
+++ gridTraveler.js
@@ -12,5 +12,21 @@
 // 4. And if the function is gridTraveler(0,1) or(1,0) or(0,0) then means the grid is empty.
 //5. Lets calculate now gridTraveler(3,3) so we want to travel in a grid with dimensions 3 by 3
 //                
-//
+// m the number of columns
 
+const gridTraveler = (m, n, memo = {}) =>{
+    const key = m + ',' + n;
+    if(key in memo)return memo[key];
+    // are the arguments in the memo stored?
+    if(m === 1 && n === 1) return 1;
+    if(m === 0 || n === 0) return 0;
+    // return gridTraveler(m - 1, n)+ gridTraveler(m, n - 1);
+    memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo);
+    return memo[key];
+};
+
+console.log(gridTraveler(1,1)); // 1
+console.log(gridTraveler(2,3)); // 3
+console.log(gridTraveler(3,2)); // 3
+console.log(gridTraveler(3,3)); // 6
+console.log(gridTraveler(19,19)); // 9075135300
