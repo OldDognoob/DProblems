3c266d949ded858c2b884a7858ca2c0dc723bcc9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-09 12:19:30 +0200

    howSum-problem

---
+++ howSum.js
@@ -0,0 +1,11 @@
+// Write a function `howSum(targetSum, numbers)` that takes in a targetSum and 
+//an array of numbers as arguments.
+// However, this function should return an array containing any combination
+// of elements that add up to exactly the targetSum. If there is no combination that
+// adds up to the targetSum, then return null.
+
+// if there are multiple combinations possible, you may return any single one.
+
+//Methodology Approach
+// This exercise is almost the same but here I want to return a combination of numbers 
+// that leads to the targetSum.
