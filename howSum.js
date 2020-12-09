ecbc5a1eac85acad82fe0be9e199449b617f205d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-09 14:02:04 +0200

    howSum-problem-solution

--- howSum.js
+++ howSum.js
@@ -10,15 +10,32 @@
 // This exercise is almost the same but here I want to return a combination of numbers 
 // that leads to the targetSum.
 
-const howSum = (targetSum, numbers)=>{
-    if(targetSum === 0)return [];
+const howSum = (targetSum, numbers,memo={})=>{
+    if(targetSum in memo)return memo[targetSum];
+    if(targetSum === 0) return [];
     if(targetSum < 0) return null;
 
     for(let num of numbers){
         const remainder = targetSum - num;
-        const remainderResult = howSum(remainder, numbers);
+        const remainderResult = howSum(remainder, numbers, memo);
         if (remainderResult !== null){
-            return [...remainderResult, num ];
+            memo[targetSum]= [...remainderResult, num ];
+            return memo[targetSum];
         }
     }
+    memo[targetSum] = null;
+    return null;
 };
+
+// m= target sum
+// n = numbers.length
+// Brute Force Approach
+// time: 0(n ^ m)
+// space: 0(m)
+
+
+console.log(howSum(7,[2,3]));// [3,2,2]
+console.log(howSum(7,[5,3,4,7]));//[4,3]
+console.log(howSum(7,[2,4]));//null
+console.log(howSum(300,[7,14]));//null
+console.log(howSum(8,[2,3,5]));//[2,2,2,2]
