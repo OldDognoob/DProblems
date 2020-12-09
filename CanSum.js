4881f2f14e1ee1059dcb4624eb1a3456e7f87697
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-09 12:05:43 +0200

    brute-force-slow-solution

--- CanSum.js
+++ CanSum.js
@@ -7,3 +7,39 @@
 //1. You may use an element of the as many times you want
 //2. You may assume that all input numbers are non negative numbers.
 
+// Methodology Approach
+
+const canSum = (targetSum, numbers) => {
+    // the function will be zero when I reach the zero by talking no numbers from the array
+if (targetSum === 0) return true;
+// adding this case scenario because in our tree we found out that there are nods we cant move any more
+// so we are if the targetSum is less than 0 return me false
+if(targetSum < 0) return false;
+
+//i m iterating through these numbers according to my recursive scenario
+// so i m iterating through every elements of the array
+for(let num of numbers){
+    // console.log(num);
+    //Having in our mind the logic from the tree earlier, we need to subtracking the current choice of number
+    // from the target number sum. This will give as the new basic target.
+    // we are using a remainder that it will take our function and the number of the array 
+    const remainder = targetSum - num;
+    // it is well known from the constrains at the top that we can use as many times we want 
+    // the numbers array.
+
+    // it actually saying in line 28 , if it is possible to generate the remainder
+    // by using the numbers of the array then we can return true for the largest problem in line 12
+    if (canSum(remainder, numbers) === true){
+        return true;
+    }
+}
+// i m returning false here because I attempt earlier to calculate all the possibilities
+// and then I found out that it is impossible to generate the targetSum.
+return false;
+};
+
+console.log(canSum(7,[2,3]));//true
+console.log(canSum(7,[5,3,4,7])); //true
+console.log(canSum(7,[2,4]));//false
+console.log(canSum(8,[2,3,5]));//true
+console.log(canSum(300,[7, 14]));//false
