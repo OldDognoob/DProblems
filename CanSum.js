d4b7c5666b150dd224b9ceac11797a62d53c119f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-09 11:42:51 +0200

    canSum-drawing-model

---
+++ CanSum.js
@@ -0,0 +1,9 @@
+//Write a function `canSum(targetSum, numbers)` that takes in a targetSum and an array of 
+// numbers as arguments.
+//This function should return a boolean indicating whether or not is is possible
+// to generate the targetSum using numbers from the array.
+
+// Constrains: 
+//1. You may use an element of the as many times you want
+//2. You may assume that all input numbers are non negative numbers.
+
